5f05e5a58e788af7147ab554edbab5cbd0625a8f
RHU4N <rhuan3003@gmail.com>
2025-09-29 10:50:06 -0300

    while

---
+++ Aula01/readline-sync/while.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+//senha
+while (true) {
+    let senha = readline.question("Digite a senha:");
+    if (senha === "1234") {
+        console.log("Senha correta. Acesso concedido.");
+        console.log("Senha correta. Acesso concedido.");
+        break;
+    }
+    else {
+        console.log("Senha incorreta. Tente novamente.");
+        console.log("Senha incorreta. Tente novamente.");
+    }
+}
